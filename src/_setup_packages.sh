0265f3ad4cff5b8b1f6ed4587e36631ff79505da
Jesse Roberts <jesse@plextrac.com>
2022-01-11 14:04:03 -0700

    better logging of apt output to enable debugging

--- src/_setup_packages.sh
+++ src/_setup_packages.sh
@@ -1,21 +1,23 @@
 function system_packages__refresh_package_lists() {
   debug "Refreshing OS package lists"
-  output=`apt-get update 2>&1` || { error "Failed to get updates"; log "$output"; return 1 ; }
+  _system_cmd_with_debug_and_fail "apt-get update 2>&1"
   debug "$output"
 }
 
 function system_packages__do_system_upgrade() {
   info "Updating OS packages, this make take some time!"
   system_packages__refresh_package_lists
-  apt-get upgrade -y -o Dpkg::Options::="--force-confold" > /dev/null 2>&1 \
-    && apt-get autoremove -y > /dev/null 2>&1
+  debug "Running system upgrade"
+  out=`apt-get upgrade -y -o Dpkg::Options::="--force-confold"  2>&1 && apt-get autoremove -y 2>&1` || { error "Failed to upgrade system packages"; debug "$out"; return 1; }
+  debug "$out"
   log "Done."
 }
 
 function system_packages__install_system_dependencies() {
   info "Installing/updating required packages..."
   system_packages__refresh_package_lists
-  apt-get install -y \
+  debug "Installing system dependencies"
+  out=`apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -23,19 +25,20 @@ function system_packages__install_system_dependencies() {
     software-properties-common \
     jq \
     unzip \
-    > /dev/null 2>&1
+    2>&1` || { error "Failed to install system dependencies"; debug "$out"; return 1; }
+  debug "$out"
   log "Done."
 }
 
 function install_docker() {
   if ! command -v docker &> /dev/null || [ "${1:-}" == "force" ]; then
     info "installing docker, this might take some time..."
-    debug "`curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 2>&1`"
+    _system_cmd_with_debug_and_fail "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 2>&1"
     debug "docker fingerprint: \n`apt-key fingerprint 0EBFCD88 2>&1`"
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /dev/null 2>&1
+    _system_cmd_with_debug_and_fail "add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" 2>&1"
     system_packages__refresh_package_lists
-    apt install -y docker-ce docker-ce-cli containerd.io > /dev/null 2>&1
-    systemctl enable docker > /dev/null 2>&1
+    _system_cmd_with_debug_and_fail "apt install -y docker-ce docker-ce-cli containerd.io 2>&1"
+    _system_cmd_with_debug_and_fail "systemctl enable docker 2>&1"
     event__log_activity "install:docker" `docker --version`
     debug `docker --version`
     log "Done."
@@ -58,4 +61,11 @@ function install_docker_compose() {
     info "docker-compose already installed, version: `docker-compose --version`"
   fi
   log "Done."
+}
+
+function _system_cmd_with_debug_and_fail() {
+  cmd=$1
+  fail_msg=${2:-"Command failed: '$cmd'"}
+  out=`eval $cmd` || { error "$fail_msg"; debug "$out"; return 1; }
+  debug "$out"
 }
