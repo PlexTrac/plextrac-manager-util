debdbb1158e3d6ba7c7a48b6ab75ffb9a3eec2b4
Jesse Roberts <jesse@plextrac.com>
2022-09-09 01:32:27 -0600

    Adding in support for postgres backup/restore, pretty basic for now

--- src/_backup.sh
+++ src/_backup.sh
@@ -5,8 +5,9 @@
 function mod_backup() {
   title "Running PlexTrac Backups"
   backup_ensureBackupDirectory
-  backup_fullUploadsBackup
+  backup_fullPostgresBackup
   backup_fullCouchbaseBackup
+  backup_fullUploadsBackup
 }
 
 function backup_fullUploadsBackup() {
@@ -16,7 +17,7 @@ function backup_fullUploadsBackup() {
   mkdir -p $uploadsBackupDir
   debug "`compose_client run --workdir /usr/src/plextrac-api --rm --entrypoint='' -T \
     $coreBackendComposeService tar -czf - uploads > \
-    ${uploadsBackupDir}/$(date "+%Y.%m.%d-%H.%M.%S").tar.gz`"
+    ${uploadsBackupDir}/$(date -u "+%Y-%m-%dT%H%M%Sz").tar.gz`"
   debug "`ls -lah ${uploadsBackupDir}`"
   log "Done."
 }
@@ -25,17 +26,33 @@ function backup_fullCouchbaseBackup() {
   info "$couchbaseComposeService: Performing backup of couchbase database"
   debug "`compose_client exec -T $couchbaseComposeService \
     chown 1337:1337 /backups 2>&1`"
-  debug "`compose_client exec -T $couchbaseComposeService \
+  debug "`compose_client exec -T --user 1337 $couchbaseComposeService \
     cbbackup -m full "http://localhost:8091" /backups -u ${CB_BACKUP_USER} -p ${CB_BACKUP_PASS} 2>&1`"
   log "Done."
 }
 
-function backup_ensureBackupDirectory() {
-  info "Ensuring backup directory exists at $PLEXTRAC_BACKUP_PATH"
-  if ! test -d "${PLEXTRAC_BACKUP_PATH}"; then
-    debug "`mkdir -vp "${PLEXTRAC_BACKUP_PATH}"`"
-  fi
+function backup_fullPostgresBackup() {
+  info "$postgresComposeService: Performing backup of postgres database"
+  backupTimestamp=$(date -u "+%Y-%m-%dT%H%M%Sz")
+  targetPath=/backups/$backupTimestamp
+  debug "`compose_client exec -T --user 1337 $postgresComposeService mkdir -p $targetPath`"
+  for db in ${postgresDatabases[@],,}; do
+    log "Backing up $db to $targetPath"
+    debug "`compose_client exec -T --user 1337 -e PGPASSWORD=$POSTGRES_PASSWORD $postgresComposeService \
+      pg_dump -U $POSTGRES_USER $db -a -f $targetPath/$db.psql 2>&1`"
+  done
+  debug "Compressing Postgres backups"
+  debug "`tar -C ${PLEXTRAC_BACKUP_PATH}/postgres/$backupTimestamp --remove-files -czvf \
+    ${PLEXTRAC_BACKUP_PATH}/postgres/$backupTimestamp.tar.gz .`"
   log "Done"
+}
+
+function backup_ensureBackupDirectory() {
+  if ! test -d "${PLEXTRAC_BACKUP_PATH}"; then
+    info "Ensuring backup directory exists at $PLEXTRAC_BACKUP_PATH"
+    debug "`mkdir -vp "${PLEXTRAC_BACKUP_PATH}"`"
+    log "Done"
+  fi
 }
 
 # function validate_backups() {
--- src/_restore.sh
+++ src/_restore.sh
@@ -4,6 +4,7 @@
 #   plextrac restore
 
 function mod_restore() {
+  restore_doPostgresRestore
   restore_doCouchbaseRestore
   restore_doUploadsRestore
 }
@@ -27,4 +28,32 @@ function restore_doCouchbaseRestore() {
   if get_user_approval; then
     compose_client exec $couchbaseComposeService cbrestore /backups http://localhost:8091 -u ${CB_BACKUP_USER} -p "${CB_BACKUP_PASS}" -x conflict_resolve=0,data_only=1
   fi
+}
+
+function restore_doPostgresRestore() {
+  title "Restoring Postgres from backup"
+  error "This is a potentially destructive process, are you sure?"
+  info "Please confirm before continuing the restore"
+  if get_user_approval; then
+    latestBackup=`ls -dc1 ${PLEXTRAC_BACKUP_PATH}/postgres/* | head -n1`
+    backupFile=`basename $latestBackup`
+    databaseBackups=$(basename -s .psql `tar -tf $latestBackup | awk '/.psql/{print $1}'`)
+    log "Restoring from $latestBackup"
+    log "Databases to restore:\n$databaseBackups"
+      debug "`compose_client exec -T --user 1337 $postgresComposeService\
+        tar -tf /backups/$backupFile 2>&1`"
+    for db in $databaseBackups; do
+      log "Extracting backup for $db"
+      debug "`compose_client exec -T $postgresComposeService\
+        tar -xvzf /backups/$backupFile ./$db.psql 2>&1`"
+      log "Restoring $db"
+      debug "`compose_client exec -T -e PGPASSWORD=$POSTGRES_PASSWORD $postgresComposeService\
+        psql -U $POSTGRES_USER -d $db -f $db.psql 2>&1`"
+      debug "`compose_client exec -T $postgresComposeService \
+        rm $db.psql 2>&1`"
+    done
+  fi
+
+
+
 }
