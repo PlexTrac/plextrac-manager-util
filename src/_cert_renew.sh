e8363cc9059b269c8f474b0c9fe2786efcbc7245
Michael Burke <michael@plextrac.com>
2023-06-23 11:14:52 -0600

    Removed ability to force recreate; retained ability to reload

--- src/_cert_renew.sh
+++ src/_cert_renew.sh
@@ -1,6 +1,6 @@
 # Build functionality for certificate renewal / injection into NGINX
 
-function mod_cert-renew() {
+function mod_reload-cert() {
   title "PlexTrac SSL Certificate Renewal"
   # Check if using LETS_ENCRYPT
   LETS_ENCRYPT_EMAIL=${LETS_ENCRYPT_EMAIL:-}
@@ -9,13 +9,9 @@ function mod_cert-renew() {
     # IF LETS_ENCRYPT = TRUE
     # ASK TO REMOVE pem/key
     info "Let's Encrypt certificate detected!"
-    info "Would you like to force renew your SSL certificate?"
+    info "Would you like to reload the SSL Certificates? This will recreate the NGINX container"
     if get_user_approval; then
-      #if yes, continue to delete pem/key and recreate NGINX
-      info "Deleting current certificates and loading new certificates"
-      compose_client rm -f plextracnginx
-      docker volume rm plextrac_letsencrypt
-      sleep 2
+      info "Recreating plextrac-plextracnginx-1"
       compose_client up -d --force-recreate plextracnginx
     else 
       die "No changes made!"
@@ -23,8 +19,8 @@ function mod_cert-renew() {
   else
     # IF LETS_ENCRYPT = FALSE
     # Assume custom_certificate key/pem has been replaced and simply re-inject via NGINX recreate
-    info "Custom or Self-signed certificate in use"
-    info "Would you like to reload your custom or self-signed certificates?"
+    info "Custom or Self-signed certificate detected!"
+    info "Would you like to reload your custom or recreate self-signed certificates?"
     if get_user_approval; then
       info "Reloading certificate..."
       compose_client up -d --force-recreate plextracnginx
