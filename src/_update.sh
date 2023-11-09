7b339744f74f1e0b1e3e049043249053eac32d32
Michael Burke <michael@plextrac.com>
2023-11-09 16:07:26 -0700

    Reworked entire structure of how it functions

--- src/_update.sh
+++ src/_update.sh
@@ -2,84 +2,6 @@
 #
 # Usage: plextrac update
 
-# Need this as a global variable
-
-upgrade_path=()
-function mod_ver_check() {
-  debug "------"
-  debug "-- Beginning version comparison"
-  # Get running version of Backend
-  running_backend_version="$(for i in `docker compose ps plextracapi -q`; do docker container inspect "$i" --format json | jq -r '(.[].Config.Labels | ."org.opencontainers.image.version")'; done | sort -u)"
-  # Set the needed JWT Token to interact with the DockerHUB API
-  JWT_TOKEN=$(wget --header="Content-Type: application/json" --post-data='{"username": "'$DOCKER_HUB_USER'", "password": "'$DOCKER_HUB_KEY'"}' -O - https://hub.docker.com/v2/users/login/ -q | jq -r .token)
-  if [ -z $JWT_TOKEN ]
-    then
-      error "Unable to retrieve JWT Token for wget, variable empty"
-    else
-      # Validate that the app is running and returning a version
-      if [[ $running_backend_version != "" ]]
-        then
-          # Get the major and minor version from the running containers
-          maj_ver=$(echo "$running_backend_version" | cut -d '.' -f1)
-          min_ver=$(echo "$running_backend_version" | cut -d '.' -f2)
-          # Statically set the version we're expecting breaking changes to begin
-          breaking_ver="1.62"
-          # Set the running version format as x.x
-          running_ver="$maj_ver.$min_ver"
-          # Default values for variables
-          page=1
-          upstream_tags=()
-          latest_ver=""
-          debug "Looking for version $running_ver"
-          while [ $page -lt 600 ]
-            do
-              # Get the available versions from DockerHub and save to array
-              upstream_tags+=(`wget --header="Authorization: JWT "${JWT_TOKEN} -O - "https://hub.docker.com/v2/repositories/plextrac/plextracapi/tags/?page=$page&page_size=1000" -q | jq -r .results[].name | grep -E '(^[0-9]\.[0-9]*$)' || true`)
-              if [ -z $latest_ver ]; then if [[ "${upstream_tags[@]}" != "" ]]; then latest_ver="${upstream_tags[0]}"; else debug "unable to fetch upstream tags"; break; fi; fi
-              if (( $(echo "$latest_ver <= $breaking_ver" | bc -l) )); then debug $breaking_ver not publically available; break; fi
-              if [[ $(echo "${upstream_tags[@]}" | grep "$breaking_ver" || true) ]]
-                then 
-                  debug "Found breaking version $breaking_ver"; break; 
-              elif [[ $(echo "${upstream_tags[@]}" | grep "$running_ver" || true) ]]
-                then
-                  debug "Found running version $running_backend_version"; break; 
-              fi
-              debug "Page of results: $page"
-              page=$[$page+1]
-          done
-          debug "-----"
-          debug "-- Listing version information"
-          debug "Upstream Versions: [${upstream_tags[@]} ]"
-          # This grabs the first element in the version sorted list which should always be the highest version available on DockerHub"
-          latest_ver="${upstream_tags[0]}"
-          debug "Breaking Version: $breaking_ver"
-          debug "Running Version: $running_ver"
-          debug "Latest Version: $latest_ver"
-          debug "Upgrade Strategy: $UPGRADE_STRATEGY"
-
-          # Check if 1.62 is avaiable to the public yet
-          if (( $(echo "$latest_ver >= $breaking_ver" | bc -l) ))
-            then
-              debug "$breaking_ver and above now available to public"
-              debug "Proceeding with contiguous update"
-              # Determine an upgrade path to follow
-              IFS=$'\n' upgrade_path=($(sort -V <<<"${upstream_tags[*]}"))
-              unset IFS
-              debug "Upgrade path: [${upgrade_path[@]}]"
-              # Variable to indicate how to handle updating
-              contiguous_update=true
-          else
-              # if breaking version isn't available, do nothing and update like normal
-              debug "Breaking version $breaking_ver not publically avaialble yet"
-              contiguous_update=false
-          fi
-        else
-          # BACKEND not running or returning version
-          debug "plextracapi not running or returning version"
-      fi
-  fi
-}
-
 # subcommand function, this is the entrypoint eg `plextrac update`
 function mod_update() {
   title "Updating PlexTrac"
@@ -95,31 +17,53 @@ function mod_update() {
     fi
   else
     info "Skipping self upgrade"
-    error "PlexTrac began/will begin doing contiguous updates to the PlexTrac application starting with the v1.62 releas. From that point forward, all releases will need to be updated with minor version increments. Skipping updating the PlexTrac Manager Util can have adverse affects on the application if a minor version update is skipped. Are you sure you want to continue skipping updates to this utility?"
+    error "PlexTrac began/will begin doing contiguous updates to the PlexTrac application starting with the v1.62 release. From that point forward, all releases will need to be updated with minor version increments. Skipping updating the PlexTrac Manager Util can have adverse affects on the application if a minor version update is skipped. Are you sure you want to continue skipping updates to this utility?"
     get_user_approval
   fi
   info "Updating PlexTrac instance to latest release..."
-  # -------- START OF NEW
-  mod_ver_check
-  debug "Contiguous update: $contiguous_update"
-  contiguous_update=true
-  debug "Number of upgrades: $(echo "${#upgrade_path[@]} - 1" | bc -l || 1)"
-  # If $contiguous_update is true
+  # Check upstream tags avaialble to download
+  version_check
   if $contiguous_update
     then
-      if [ $UPGRADE_STRATEGY == "stable" ]; then info "stable is the chosen"; fi
-      info "Running Ver is $running_ver"
       debug "Proceeding with contiguous update"
+      upgrade_time_estimate
       for i in ${upgrade_path[@]}
          do
             if [ "$i" != "$running_ver" ]
               then
-                info "Upgrading to $i"
+                debug "Upgrading to $i"
+                mod_configure
                 UPGRADE_STRATEGY="$i"
-                info "UPGRADE_STRATEGY is now $UPGRADE_STRATEGY"
+                debug "Upgrade Strategy is $UPGRADE_STRATEGY"
+                title "Pulling latest container images"
+                exit
+                pull_docker_images
+                if [ "$IMAGE_CHANGED" == true ]
+                  then
+                    title "Executing Rolling Deployment"
+                    mod_rollout
+                fi
+                # Sometimes containers won't start correctly at first, but will upon a retry
+                maxRetries=2
+                for i in $( seq 1 $maxRetries ); do
+                  mod_start || sleep 5 # Wait before going on to health checks, they should handle triggering retries if mod_start errors
+                  unhealthy_services=$(compose_client ps -a --format json | \
+                    jq -r '. | select(.Health == "unhealthy" or (.State != "running" and .ExitCode != 0) or .State == "created" ) | .Service' | \
+                    xargs -r printf "%s;")
+                  if [[ "${unhealthy_services}" == "" ]]; then break; fi
+                  info "Detected unhealthy services: ${unhealthy_services}"
+                  if [[ $i -ge $maxRetries ]]; then
+                    error "One or more containers are in a failed state, please contact support!"
+                    exit 1
+                  fi
+                  info "An error occurred with one or more containers, attempting to start again"
+                  sleep 5
+                done
             fi
       done
-    else
+      mod_check
+      title "Update complete"
+  else
       debug "Proceeding with normal update"
       mod_configure
       title "Pulling latest container images"
---
+++ src/_version_check.sh
@@ -0,0 +1,175 @@
+# Need this as a global variable
+upgrade_path=()
+
+function upgrade_time_estimate() {
+  debug "upgrade_time_estimate function running"
+  if (( ${#upgrade_path[@]} >= 1 ))
+    then
+      time_estimate=$(echo "${#upgrade_path[@]} * 15" | bc -l)
+      error "Detected ${#upgrade_path[@]} upgrade(s). Each upgrade can take up to 15 minutes to pull the new version, and update the running application. Given the number of upgrades, the projected upgade time is $time_estimate. Are you sure you want to continue?"
+      get_user_approval
+  fi
+}
+
+function upgrade_warning() {
+  debug "upgrade_warning function running"
+  error "Its been detected that you're on a pinned version of PlexTrac other than stable. Beginning with version 1.62, PlexTrac is going to require contiguous updates to ensure code migrations are successful and enable us to continue to move forward with improving the platform."
+  error "Are you sure you want to update to $UPGRADE_STRATEGY?"
+  get_user_approval
+}
+
+function version_check() {
+  #######################
+  ### -- Running Version
+  #######################
+  ## LOGIC: RunVer
+  title "Running Version"
+  # Get running version of Backend
+  running_backend_version="$(for i in $(docker compose ps plextracapi -q); do docker container inspect "$i" --format json | jq -r '(.[].Config.Labels | ."org.opencontainers.image.version")'; done | sort -u)"
+
+  # CONDITION: plextracapi IS/NOT RUNNING
+  # Validate that the app is running and returning a version
+  if [[ $running_backend_version != "" ]]; then
+    debug "RunVer: plextracapi is running and is version $running_backend_version"
+    # Get the major and minor version from the running containers
+    maj_ver=$(echo "$running_backend_version" | cut -d '.' -f1)
+    min_ver=$(echo "$running_backend_version" | cut -d '.' -f2)
+    # Set the running version format as x.x
+    running_ver="$maj_ver.$min_ver"
+  else
+    debug "RunVer: plextracapi is NOT running"
+    die "plextracapi service isn't running. Please run 'plextrac start' and re-run the update"
+  fi
+
+  #######################
+  ### -- Pinned Version
+  #######################
+  ## LOGIC: PinVer
+  title "Pinned Version"
+  # Check what the pinned version (UPGRADE_STRATEGY) is and see if a contiguous update will apply
+  ## IF STABLE
+  if [[ "$UPGRADE_STRATEGY" == "stable" ]]; then
+    debug "PinVer: Running Stable!"
+
+    #######################
+    ### -- Latest Stable Version
+    #######################
+    title "Latest_Stable Version"
+    # Set vars
+    breaking_ver="1.62"
+    latest_ver=""
+    page=1
+
+    # Set the needed JWT Token to interact with the DockerHUB API
+    # TODO: What is JWT is empty or in Airgapped / on-prem env?
+    JWT_TOKEN=$(wget --header="Content-Type: application/json" --post-data='{"username": "'$DOCKER_HUB_USER'", "password": "'$DOCKER_HUB_KEY'"}' -O - https://hub.docker.com/v2/users/login/ -q | jq -r .token)
+
+    # Get latest from DockerHUB and assign to array
+    while [ $page -lt 600 ]; do
+      latest_ver=($(wget --header="Authorization: JWT "${JWT_TOKEN} -O - "https://hub.docker.com/v2/repositories/plextrac/plextracapi/tags/?page=$page&page_size=1000" -q | jq -r .results[].name | grep -E '(^[0-9]\.[0-9]*$)' || true))
+      page=$(($page + 1))
+      if [ -n $latest_ver ]; then break; fi
+    done
+    # Set latest_var to first index item which should be the "latest"
+    latest_ver="${latest_ver[0]}"
+
+    ### Compare stable and latest
+    # Get date stable was pushed
+    stable_date=$(date -d $(wget --header="Authorization: JWT "${JWT_TOKEN} -O - "https://hub.docker.com/v2/repositories/plextrac/plextracapi/tags/stable" -q | jq -r .tag_last_pushed) +%s)
+    # Get date for the latest version tag was pushed
+    latest_date=$(date -d $(wget --header="Authorization: JWT "${JWT_TOKEN} -O - "https://hub.docker.com/v2/repositories/plextrac/plextracapi/tags/$latest_ver" -q | jq -r .tag_last_pushed) +%s)
+    
+    ## LOGIC: LATEST_STABLE
+    # IF LATEST_STABLE <= 1.62
+    if (( $(echo "$latest_ver <= $breaking_ver" | bc -l) ))
+      then 
+        debug "$breaking_ver not publically available. Updating normally without warning"
+        contiguous_update=false
+      
+      # IF LATEST_STABLE >= 1.63
+      else
+        debug "stable version is greater than $breaking_ver. Running contiguous update"
+        contiguous_update=true
+    fi
+
+    # TEST
+    contiguous_update=true
+    upstream_tags=()
+    page=1
+    if [ "$contiguous_update" = true ]
+      then
+        # Get upstream tag list
+        debug "Looking for Running version $running_ver or Breaking version $breaking_ver"
+        while [ $page -lt 600 ]
+          do
+            # Get the available versions from DockerHub and save to array
+            if [[ $(echo "${upstream_tags[@]}" | grep "$breaking_ver" || true) ]]
+              then
+                debug "Found breaking version $breaking_ver"; break;
+            elif [[ $(echo "${upstream_tags[@]}" | grep "$running_ver" || true) ]]
+              then
+                debug "Found running version $running_backend_version"; break;
+            else
+              upstream_tags+=(`wget --header="Authorization: JWT "${JWT_TOKEN} -O - "https://hub.docker.com/v2/repositories/plextrac/plextracapi/tags/?page=$page&page_size=1000" -q | jq -r .results[].name | grep -E '(^[0-9]\.[0-9]*$)' || true`)
+            fi
+            debug "Page of results: $page"
+            page=$[$page+1]
+        done
+        # Compare stable vs latest_tag and if stable is older than the newest tag, then remove the newest tag to ensure we don't update past stable
+        if [ $stable_date -le $latest_date ]; then
+          debug "Stable is older than latest version"
+          # If stable is older, remove latest from upgrade path
+          for i in ${!upstream_tags[@]}
+            do
+              if [[ ${upstream_tags[i]} = $latest_ver ]]
+                then
+                  debug "correcting upstream_tags to remove latest"
+                  unset 'upstream_tags[i]'
+              fi
+          done
+        else
+          debug "Stable is pinned to the latest version $latest_ver"
+        fi
+        
+        # Remove the running version from the Upgrade path
+        for i in ${!upstream_tags[@]}
+          do
+            if [[ ${upstream_tags[i]} = $running_ver ]]
+              then
+                debug "correcting upstream_tags to remove running version"
+                unset 'upstream_tags[i]'
+            fi
+        done
+        for i in "${!upstream_tags[@]}"; do
+            new_array+=( "${upstream_tags[i]}" )
+        done
+        upstream_tags=("${new_array[@]}")
+        unset new_array
+        debug "New upgrade path: ${upstream_tags[@]}"
+        # This grabs the first element in the version sorted list which should always be the highest version available on DockerHub; this should match stable's version"
+        latest_ver="${upstream_tags[0]}"
+        TIFS=$'\n' upgrade_path=($(sort -V <<<"${upstream_tags[*]}"))
+        unset TIFS
+        debug "------------"
+        debug "Listing version information"
+        debug "------------"
+        debug "Upgrade Strategy: $UPGRADE_STRATEGY"
+        debug "Running Version: $running_ver"
+        debug "Breaking Version: $breaking_ver"
+        debug "Upstream Versions: [${upstream_tags[@]}]"
+        debug "Latest Version: $latest_ver"
+        debug "Upgrade path: [${upgrade_path[@]}]"
+        debug "Number of upgrades: ${#upgrade_path[@]}"
+      # TODO: How to handle NON-CONTIGUOUS Update
+      else
+        debug "Else"
+    fi
+  ## IF NOT STABLE
+  else
+    # Running Pinned Version; Normal update with warning
+    debug "PinVer: Running as a non-stable, pinned version -- proceed with warning and update"
+    contiguous_update=false
+    # TODO: warning function
+    upgrade_warning
+  fi
+}
