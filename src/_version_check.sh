db08548e474e30358d8888808bb24cde6463336f
Michael Burke <72173919+Michael-Burke@users.noreply.github.com>
2024-02-26 09:51:05 -0700

    Fixed and updated logic for upstream tag (#113)
    Fixed and updated logic for upstream tag to [[]] instead of []

--- src/_version_check.sh
+++ src/_version_check.sh
@@ -144,7 +144,7 @@ function version_check() {
           # Set Contiguous updates to false here to ensure that since the app is on latest version, it still attempts to pull patch version updates
           contiguous_update=false
         fi
-        if [ "${upstream_tags[@]}" != "" ]; then
+        if [[ "${upstream_tags[@]}" != "" ]]; then
                 # Sort the upstream tags weve chosen as the upgrade path
                 IFS=$'\n' upgrade_path=($(sort -V <<<"${upstream_tags[*]}"))
                 # Reset IFS to default value
