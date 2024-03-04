dcf9efbad1007dec9e778deb2113ce9c3f44f146
Mike Morgan <mmorgan@plextrac.com>
2024-03-04 13:55:27 -0700

    reduce logging level, handle missing npm module, use modern subshell notation

--- src/_configure_plextrac.sh
+++ src/_configure_plextrac.sh
@@ -107,16 +107,15 @@ function setDefaultSecrets() {
 function getCKEditorRTCConfig() {
   # parses output and saves the result of the json meta data
   # the last line, which only contains the JSON data, should be used
-  CKEDITOR_JSON=`compose_client exec plextracapi npm run ckeditor:environment:migration | grep '^{' || log "ERROR: Unable to run ckeditor:environment:migration"`
+  CKEDITOR_JSON=$(compose_client exec plextracapi npm run ckeditor:environment:migration --if-present | grep '^{' || debug "ERROR: Unable to run ckeditor:environment:migration")
 
   # check the result to confirm it contains the expected element in the JSON, then base64 encode if it does
-  if [ `echo $CKEDITOR_JSON | jq -e ".[]|any(\".api_secret\")"` ]; then
+  if [ $(echo $CKEDITOR_JSON | jq -e ".[]|any(\".api_secret\")") ]; then
     CKEDITOR_SERVER_CONFIG=`echo $CKEDITOR_JSON | base64 -w 0`
     echo -n "CKEDITOR_SERVER_CONFIG=${CKEDITOR_SERVER_CONFIG}"
-    log "Done."
   else
     echo -n "CKEDITOR_SERVER_CONFIG="
-    log "ERROR: Unable to generate CKEditor RTC service config, setting to empty string"
+    debug "ERROR: Unable to generate CKEditor RTC service config, setting to empty string"
   fi
 }
 
