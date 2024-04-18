29f513cd7a1a2e0710579e346dc5654a9315f5d8
Michael Burke <michael@plextrac.com>
2024-04-18 15:33:16 -0600

    Adjusted ids referenced to match expected id -u output

--- src/_backup.sh
+++ src/_backup.sh
@@ -34,7 +34,7 @@ function backup_fullUploadsBackup() {
     podman exec --workdir="/usr/src/plextrac-api/uploads" plextracapi rm $current_date.tar.gz
     debug "Cleaned Archive from container"
   else
-    debug "`compose_client run --user 1337 -v ${uploadsBackupDir}:/backups \
+    debug "`compose_client run --user $(id -u) -v ${uploadsBackupDir}:/backups \
       --workdir /usr/src/plextrac-api --rm --entrypoint='' -T  $coreBackendComposeService \
       tar -czf /backups/$(date -u "+%Y-%m-%dT%H%M%Sz").tar.gz uploads`"
   fi
@@ -43,12 +43,12 @@ function backup_fullUploadsBackup() {
 
 function backup_fullCouchbaseBackup() {
   info "$couchbaseComposeService: Performing backup of couchbase database"
-  local cmd='compose_client exec -T --user 1337'
+  local user_id=$(id -u plextrac)
+  local cmd="compose_client exec -T --user $user_id"
   if [ "$CONTAINER_RUNTIME" == "podman" ]; then
     cmd='docker exec'
   fi
   if [ "$CONTAINER_RUNTIME" != "podman" ]; then
-  local user_id=$(id -u plextrac)
     debug "`$cmd $couchbaseComposeService \
       chown -R $user_id:$user_id /backups 2>&1`"
   fi
@@ -63,11 +63,11 @@ function backup_fullCouchbaseBackup() {
 
 function backup_fullPostgresBackup() {
   info "$postgresComposeService: Performing backup of postgres database"
-  local cmd='compose_client exec -T --user 1337'
+  local user_id=$(id -u plextrac)
+  local cmd="compose_client exec -T --user $user_id"
   if [ "$CONTAINER_RUNTIME" == "podman" ]; then
     cmd='docker exec'
   fi
-  local user_id=$(id -u plextrac)
   if [ "$CONTAINER_RUNTIME" != "podman" ]; then
     debug "`compose_client exec -T $postgresComposeService chown -R $user_id:$user_id /backups 2>&1`"
   fi
--- src/_restore.sh
+++ src/_restore.sh
@@ -55,7 +55,7 @@ function restore_doCouchbaseRestore() {
     if [ "$CONTAINER_RUNTIME" == "podman" ]; then
       podman exec --workdir /backups $couchbaseComposeService tar -xzvf /backups/$backupFile
     else
-      debug "`compose_client exec -T --user 1337 --workdir /backups $couchbaseComposeService \
+      debug "`compose_client exec -T --user $(id -u plextrac) --workdir /backups $couchbaseComposeService \
         tar -xzvf /backups/$backupFile 2>&1`"
     fi
 
@@ -75,7 +75,7 @@ function restore_doCouchbaseRestore() {
     if [ "$CONTAINER_RUNTIME" == "podman" ]; then
       podman exec --workdir /backups $couchbaseComposeService rm -rf /backups/$dirName
     else
-      debug "`compose_client exec -T --user 1337 --workdir /backups $couchbaseComposeService \
+      debug "`compose_client exec -T --user $(id -u plextrac) --workdir /backups $couchbaseComposeService \
         rm -rf /backups/$dirName 2>&1`"
     fi
     log "Done"
@@ -95,7 +95,7 @@ function restore_doPostgresRestore() {
     databaseBackups=$(basename -s .psql `tar -tf $latestBackup | awk '/.psql/{print $1}'`)
     log "Restoring from $backupFile"
     log "Databases to restore:\n$databaseBackups"
-    local cmd='compose_client exec -T --user 1337'
+    local cmd="compose_client exec -T --user $(id -u plextrac)"
     if [ "$CONTAINER_RUNTIME" == "podman" ]; then
       local cmd='podman exec'
     fi
