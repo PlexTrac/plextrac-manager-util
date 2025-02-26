e5da42de563d8669cc6024d001c4e949b2255f55
Mike Morgan <mmorgan@plextrac.com>
2025-02-26 16:42:14 -0700

    add new compose files and logic to handle new services and upgrades

--- src/_configure_plextrac.sh
+++ src/_configure_plextrac.sh
@@ -75,6 +75,13 @@ CLOUD_STORAGE_ACCESS_KEY=${CLOUD_STORAGE_ACCESS_KEY:-`generateSecret 20`}
 CLOUD_STORAGE_SECRET_KEY=${CLOUD_STORAGE_SECRET_KEY:-`generateSecret`}
 CLOUD_STORAGE_ENDPOINT=${CLOUD_STORAGE_ENDPOINT:-"minio"}
 CLOUD_STORAGE_SSL=${CLOUD_STORAGE_SSL:-"false"}
+CKEDITOR_SERVER_LICENSE_KEY=${CKEDITOR_SERVER_LICENSE_KEY:-}
+IMAGE_REGISTRY=docker.cke-cs.com
+IMAGE_REGISTRY_PASS=${IMAGE_REGISTRY_PASS:-}
+IMAGE_REGISTRY_USER=cs
+`setCKEMigrateConfig`
+CKEDITOR_ENABLE_METRIC_LOGS=false
+CKEDITOR_LOG_LEVEL=40
 
 
 `generate_default_couchbase_env | setDefaultSecrets`
@@ -127,6 +134,14 @@ function setDefaultSecrets() {
     #echo "$var=${var:-$val}"
   done < "${1:-/dev/stdin}"
   export IFS=$OLDIFS
+}
+
+function setCKEMigrateConfig() {
+  if [ -z ${CKEDITOR_SERVER_LICENSE_KEY} ]; then
+    echo CKEDITOR_MIGRATE=false
+  else
+    echo CKEDITOR_MIGRATE=true
+  fi
 }
 
 function login_dockerhub() {
@@ -186,8 +201,20 @@ function updateComposeConfig() {
   docker_createInitialComposeOverrideFile
   targetComposeFile="${PLEXTRAC_HOME}/docker-compose.yml"
 
+  # If upgrade strategy is stable or greater than 2.0, we should use the newer compose file
+  # If we also see the ckeditor RTC license key, we use the compose file with CKE RTC services
+  if [ "${UPGRADE_STRATEGY}" == "stable" ] || [ $(printf "%03d%03d%03d%03d" $(echo "${UPGRADE_STRATEGY}" | tr '.' ' ')) -ge $(printf "%03d%03d%03d%03d" $(echo "2.0" | tr '.' ' ')) ]; then
+    if [ -n "${CKEDITOR_SERVER_LICENSE_KEY}" ]; then
+      decodedComposeFile=$(base64 -d <<<$DOCKER_COMPOSE_ENCODED_V2_RTC)
+    else
+      decodedComposeFile=$(base64 -d <<<$DOCKER_COMPOSE_ENCODED_V2)
+    fi
+  else
+    decodedComposeFile=$(base64 -d <<<$DOCKER_COMPOSE_ENCODED)
+  fi
+
   info "Checking $targetComposeFile for changes"
-  decodedComposeFile=$(base64 -d <<<$DOCKER_COMPOSE_ENCODED)
+
   if ! test -f "$targetComposeFile"; then
     debug "Creating initial file"
     echo "$decodedComposeFile" > $targetComposeFile
