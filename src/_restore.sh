eaee4edc771341d1a0459bac11b72608f9d1dfc0
Nick Caswell <ncaswell@plextrac.com>
2026-03-24 16:31:40 -0400

    modifying restore to account for K3s format of backup

--- src/_restore.sh
+++ src/_restore.sh
@@ -54,32 +54,38 @@ function restore_doCouchbaseRestore() {
 
   if get_user_approval; then
     log "Restoring from $backupFile"
+    # Create a unique temp directory for extraction
+    timestamp=$(date +%s)
+    restoreTmp="/backups/restore_tmp_$timestamp"
+
     log "Extracting backup files"
     if [ "$CONTAINER_RUNTIME" == "podman" ]; then
-      podman exec --workdir /backups $couchbaseComposeService tar -xzvf /backups/$backupFile
+      podman exec $couchbaseComposeService mkdir -p $restoreTmp
+      podman exec $couchbaseComposeService tar -C $restoreTmp -xzvf /backups/$backupFile
     else
-      debug "`compose_client exec -T --user $(id -u ${PLEXTRAC_USER_NAME:-plextrac}) --workdir /backups $couchbaseComposeService \
-        tar -xzvf /backups/$backupFile 2>&1`"
+      debug "`compose_client exec -T --user $(id -u ${PLEXTRAC_USER_NAME:-plextrac}) $couchbaseComposeService \
+        mkdir -p $restoreTmp 2>&1`"
+      debug "`compose_client exec -T --user $(id -u ${PLEXTRAC_USER_NAME:-plextrac}) $couchbaseComposeService \
+        tar -C $restoreTmp -xzvf /backups/$backupFile 2>&1`"
     fi
 
     log "Running database restore"
     if [ "$CONTAINER_RUNTIME" == "podman" ]; then
-      podman exec $couchbaseComposeService cbrestore /backups http://127.0.0.1:8091 \
+      podman exec $couchbaseComposeService cbrestore $restoreTmp http://127.0.0.1:8091 \
         -u ${CB_BACKUP_USER} -p "${CB_BACKUP_PASS}" --from-date 2022-01-01 -x conflict_resolve=0,data_only=1
     else
       # We have the TTY enabled by default so the output from cbrestore is intelligible
       tty -s || { debug "Disabling TTY allocation for Couchbase restore due to non-interactive invocation"; ttyFlag="-T"; }
-      compose_client exec ${ttyFlag:-} $couchbaseComposeService cbrestore /backups http://127.0.0.1:8091 \
+      compose_client exec ${ttyFlag:-} $couchbaseComposeService cbrestore $restoreTmp http://127.0.0.1:8091 \
         -u ${CB_BACKUP_USER} -p "${CB_BACKUP_PASS}" --from-date 2022-01-01 -x conflict_resolve=0,data_only=1
     fi
 
     log "Cleaning up extracted backup files"
-    dirName=`basename -s .tar.gz $backupFile`
     if [ "$CONTAINER_RUNTIME" == "podman" ]; then
-      podman exec --workdir /backups $couchbaseComposeService rm -rf /backups/$dirName
+      podman exec $couchbaseComposeService rm -rf $restoreTmp
     else
-      debug "`compose_client exec -T --user $(id -u ${PLEXTRAC_USER_NAME:-plextrac}) --workdir /backups $couchbaseComposeService \
-        rm -rf /backups/$dirName 2>&1`"
+      debug "`compose_client exec -T --user $(id -u ${PLEXTRAC_USER_NAME:-plextrac}) $couchbaseComposeService \
+        rm -rf $restoreTmp 2>&1`"
     fi
     log "Done"
   fi
@@ -142,21 +148,29 @@ function restore_doPostgresRestore() {
     fi
 
     # now actually perform the db restore
-    databaseBackups=$(basename -s .psql `tar -tf $latestBackup | awk '/.psql/{print $1}'`)
-    log "Restoring from $backupFile"
-    log "Databases to restore:\n$databaseBackups"
-    local cmd="compose_client exec -T --user $(id -u ${PLEXTRAC_USER_NAME:-plextrac})"
-    if [ "$CONTAINER_RUNTIME" == "podman" ]; then
-      local cmd='podman exec'
+    # Extract backup to a temp directory to handle varying tarball structures
+    timestamp=$(date +%s)
+    restoreTmp="/tmp/postgres_restore_$timestamp"
+    mkdir -p $restoreTmp
+    tar -xvzf $latestBackup -C $restoreTmp
+
+    # Find all .psql files in the extracted backup, regardless of directory structure
+    mapfile -t psql_files < <(find $restoreTmp -type f -name '*.psql')
+    if [ ${#psql_files[@]} -eq 0 ]; then
+      error "No .psql files found in backup archive."
+      rm -rf $restoreTmp
+      return 1
     fi
-      debug "`$cmd $postgresComposeService \
-        tar -tf /backups/$backupFile 2>&1`"
+
+    log "Restoring from $backupFile"
+    log "Databases to restore:\n${psql_files[*]}"
     local cmd='compose_client exec -T'
     if [ "$CONTAINER_RUNTIME" == "podman" ]; then
       local cmd='podman exec'
     fi
-    for db in $databaseBackups; do
-      log "Extracting backup for $db"
+    for psql_path in "${psql_files[@]}"; do
+      db=$(basename "$psql_path" .psql)
+      log "Restoring backup for $db"
       # only the core database gets timescaledb tables, so we need to do special things for this restore to work
       if [ $db = "core" ]; then
         log "restoring core db, running special timescaledb commands"
@@ -184,8 +198,6 @@ function restore_doPostgresRestore() {
         fi
       fi
 
-      debug "`$cmd $postgresComposeService\
-        tar -xvzf /backups/$backupFile ./$db.psql 2>&1`"
       dbAdminEnvvar="PG_${db^^}_ADMIN_USER"
       dbAdminRole=$(eval echo "\$$dbAdminEnvvar")
 
@@ -195,11 +207,18 @@ function restore_doPostgresRestore() {
       # run as the first command in the session due to the way it modified the process's memory.
       dbRestoreFlags="-d $db --no-privileges --no-owner --role=$dbAdminRole  --disable-triggers --verbose"
 
+      # Copy the backup file into the container for restore
+      if [ "$CONTAINER_RUNTIME" == "podman" ]; then
+        podman cp "$psql_path" $postgresComposeService:/tmp/$(basename "$psql_path")
+      else
+        docker cp "$psql_path" $(docker compose ps -q $postgresComposeService):/tmp/$(basename "$psql_path")
+      fi
+
       log "Restoring $db with role:${dbAdminRole}"
       debug "`$cmd -e PGPASSWORD=$POSTGRES_PASSWORD $postgresComposeService \
-        pg_restore -U $POSTGRES_USER $dbRestoreFlags ./$db.psql 2>&1`"
+        pg_restore -U $POSTGRES_USER $dbRestoreFlags /tmp/$db.psql 2>&1`"
       debug "`$cmd $postgresComposeService \
-        rm ./$db.psql 2>&1`"
+        rm /tmp/$db.psql 2>&1`"
 
       # Run through the post-restore steps for core db
       if [ $db = "core" ]; then
@@ -218,6 +237,9 @@ function restore_doPostgresRestore() {
         fi
       fi
     done
+
+    # Clean up the temp extraction directory
+    rm -rf $restoreTmp
 
     log "now, start the rest of the app and sleep for 120s to give couchbase a chance"
     mod_start
